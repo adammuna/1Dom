5c6a70a5066febc1042c6502d1d35d8df0245e9f
iMac 2 <imac2@iMac2.local>
2022-03-03 17:30:11 +0200

    a

---
+++ main.js
@@ -0,0 +1,257 @@
+// const limit = 10
+// let count = 0
+// let stars = "*"
+// while (count <=limit){
+// console.log(stars);
+// stars += "*"
+// count++
+// }
+// const str = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, distinctio iusto cupiditate soluta numquam fugit possimus quo, quasi asperiores quam officiis, minima blanditiis neque aliquid quibusdam libero? Cupiditate, odit at.'
+//  for (let i = 0; str[i]  ; i++) {
+//  console.log(str[i]);  
+// }
+// let sum = char + '5'
+// console.log(sum);
+// if (sum) {
+//     console.log('truthy');
+// }
+// else {
+//     console.log('falsy');
+// }
+
+// let str = "As sQ dQ fE gF hU jk kQ"
+// let count = 0 
+// for (let i = 0; str[i]; i++){
+//     if (str[i] == str[1].charCodeAt()) {
+//         count++
+
+//     }
+// }
+//  console.log(count);
+
+// let str = "zqwsdGKNCUDSF"
+// let saqw = 0
+// for (let i = 0; str[i]; i++) {
+//    if (str.charCodeAt(i) >= "65" && str.charCodeAt(i) <= "90"){
+
+//     saqw++
+//    }
+// }
+// console.log(saqw);
+// const sum = function (n1,n2){
+// const a = n1 + n2
+// return sum
+
+// }
+// console.log(sum(4,4)
+  
+// );
+// let a = 22223dfswsdsa2$2!46$
+// let b = ($,!)
+
+// const count = function (str , special ) {
+   
+// }
+// console.log(function);
+
+// const count = function (str, result) {
+// sum = 1
+// num1 = num
+
+// for (let w = num * num1   
+//   y = function (a , re) {
+//  let y = a 
+
+//  let num = 1
+//  i -- == sum;
+// console.log(str8)
+// } 
+// console.log(result)
+// const str = "apple #cat#"
+// const find = function (str) { 
+//    let find =" "
+// for (let i = 0; i < str[w] ; i++) {
+//    const element = [index];
+// if (str) == "#cat#"
+// (console.log(cat))   
+// }
+//    return str
+// }
+// const str = "apple #cat#"
+// const findkey = function (str){
+//  let key = ""
+//  let reading = false
+// for (let i = 0; i < str.length; i++) {
+//    if (str[i] == "#")
+//    { reading == !reading
+
+//    }
+//    if (reading && str[i]!='#'){
+//       key +=str[i] 
+//    }
+// return key
+   
+// }
+// }
+// console.log(
+//    findkey(str)
+// );
+// const replacekey = function (str , value ){}
+// let newstring = " "
+// let isreading = false
+// let findkey=false
+// for (let i = 0; i < str[i]; i++) {
+//   if (str[i] == "#"){
+//    isreading = !isreading
+//    findkey=false
+//   }
+//   if  (isreading){
+//      if (!findkey){
+//        newstring += value
+//        findkey = true 
+//      }
+//      else{
+
+//       newstring +=str[i] == "#" ? " " : str[i]
+//      }
+
+//    }
+//    console.log(newstring);
+//    return newstring
+// 
+// const arry = ["adam" , "qq" ,"yazzed"]
+
+// const sayhello = function (arr) {
+//    for (let i = 0; index < arr.length; i++) {
+//       console.log("hello" + arr[i] );
+//       sayhello(arry)
+//       sayhello(["rami" , "adam"])
+
+      
+//    }
+   
+// // }
+// const num1 = [4,5,6]
+// const num2 = num1
+// const num3 = num2
+
+// num3[0]= 9
+// console.log(num2);
+// console.log(num1);
+
+// const arr5 =['ahmad', "yazzed","Adam"]
+// const formatarray = function (arr) {
+//    for (let i = 0; i < arr.length; i++) {
+//       arr[i] = "hello"+ arr[i];
+      
+//    }
+   
+// }
+// formatarray(arr5)
+// console.log(arr5)
+// const arr2 = ["adam" , "yazed" , "ahmad"]
+// let capitalize = function (arr2) { 
+// for (let i = 0; i < sen.length; i++) {
+//    const senarr = sen.split(' ')
+//    let capitalize = arr2.toUppercase
+
+   
+   
+// }
+// capitalize(arr2)
+// console.log(arr2);
+
+   
+// }
+// const arr3 = ["ahmad", "sameer", "rami"]
+// const Capitalize = function (sen) {
+//    for (let i = 0; index < arr3.length; index++) {
+      
+     
+      
+//    }
+   
+// }
+// const  arr1 = ['ahmad','ibrahim' , 'bahaa' , 'khaled', 'yazeed','adam']
+// console.log(arr1);
+
+
+// // arr1.push('ahmad')
+// // arr1.slice()
+// // arr1.splice()
+// arr1.pop()
+// const  arr1 = ['ahmad','ibrahim' , 'bahaa' , 'khaled', 'yazeed','adam']
+// arr1.length
+// const user = {Firstname :'ahmad' , Lastname : 'kawasmi', age:42 }
+// user.mobilephone  = '0522723456';
+// const newuser = user
+// newuser = {Firstname : 'bahaa'}
+// console.log(user);
+// console.log(newuser);   
+// const user = {
+// Firstname : "ahmad",
+// Lastname : 'kawasmi',
+// age : 42,
+// isloggedIn : false,
+// signIN : function (){
+//    this.isloggedIn = true
+// },
+// user.signIN()
+// console.log(user);
+
+
+
+
+// }
+const title = document.getElementById("title")
+const subHeader = document.getElementsByTagName('h2')
+const uniqueHeaders = document.getElementsByClassName('unique')
+const header = document.querySelectorAll('#superUnique')
+const body = document.getElementsByTagName("body")[0]
+const input = document.querySelector('#myInput')
+const btn = document.getElementById("myBtn")
+
+
+
+const CreatePost = function () {
+      const text = input.value
+      const post = document.createElement('h1')
+      const hr = document.createElement('hr')
+      post.innerText =text
+      body.append(post,hr)
+      input.value = ''
+   
+}
+btn.addEventListener('click' , function(){
+
+
+})
+
+ btn.addEventListener('click',creatpost)
+
+
+ input.addEventListener('keydown',function (event) {
+   if (event.key == "enterkey") {
+      creatpost()
+   }
+   
+})
+  
+
+const dynamicelemnt = document.createElement('div')
+dynamicelemnt.id = "dynamicEle"
+dynamicelemnt.classList.add('dynamic')
+body.append(dynamicelemnt)   
+
+
+
+
+
+   
+
+
+
+  
+  
+  
+
